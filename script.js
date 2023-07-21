f74daab7e3fd7f1c0093927a755c8f20ce9faa65
jjpound <140023964+jjpound@users.noreply.github.com>
2023-07-20 22:18:24 -0700

    First interactivity Update
    Buttons make a transition-animation to open up different sections. Next step is to make the sections transition in color between each other.

--- script.js
+++ script.js
@@ -1,10 +1,49 @@
 console.log("Wowie!");
-let aboutSection = document.getElementById("about-me");
+let unselectedSections = document.getElementsByClassName("unselected");
+let selectedSections = document.getElementsByClassName("selected");
+let mainPageButtons = document.getElementsByClassName("mainpage-content-opener");
+let hiddenButtons = document.getElementsByClassName("hidden");
 
-function openAboutMe() {
-     aboutSection.classList.replace("unselected", "selected");
+function hideButtons() {
+    for(i = 0; i<4; i++) {
+        mainPageButtons[0].classList.replace("mainpage-content-opener", "hidden");
+    }
 }
 
-function closeSelectedWindow() {
-    aboutSection.classList.replace("selected", "unselected");
+function showButtons() {
+    for(i = 0; i<4; i++) {
+        hiddenButtons[0].classList.replace("hidden", "mainpage-content-opener");
+    }
+}
+
+function openSection(section) {
+    hideButtons()
+    unselectedSections[section].classList.replace("unselected", "selected");
+    for(i = 0; i<3; i++) {
+        unselectedSections[i].removeAttribute("animate");
+    }
+}
+
+function closeSelectedWindow(section) {
+    selectedSections[section].setAttribute("closing", "");
+    selectedSections[section].addEventListener(
+        "animationend",
+        () => {
+            selectedSections[section].removeAttribute("closing");
+            selectedSections[section].classList.replace("selected", "unselected");
+        },
+        { once: true }
+    );
+    showButtons();
+    for(i=0; i<3; i++) {
+        unselectedSections[i].setAttribute("animate", "");
+        selectedSections[0].setAttribute("animate", "");
+    }
+}
+
+function replaceSelectedWindow(section) {
+    selectedSections[0].removeAttribute("animate");
+    selectedSections[0].classList.replace("selected", "unselected");
+    unselectedSections[section].classList.replace("unselected", "selected");
+    unselectedSections[section].setAttribute("animate", "");
 }
